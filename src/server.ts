8a0aa833a101365c4d129ec2c2e307eaf49b322d
apfdamascena <apfd@cin.ufpe.br>
2023-07-22 13:56:20 -0300

    feature(base-project): add mongobd

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { env } from "./env";
   try {
     app.listen(env.PORT, env.IPV4, async () => {
       const message = `[Server]: Server ready at port ${env.PORT}`;
+      console.log(message)
     });
   } catch (error) {
     console.log(`[Receitinha-Server]: failed to connect.`);
