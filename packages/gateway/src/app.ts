2c5ad4afa875283661682dc3fa50f9d1a111cfac
Sofia Melo <sofianovica@hotmail.com>
2023-08-27 18:09:26 -0300

    extra: remove node-modules

--- packages/gateway/src/app.ts
+++ packages/gateway/src/app.ts
@@ -1,14 +1,21 @@
 import express, { Request, Response } from 'express'
 import cors from "cors" 
 import httpProxy from "express-http-proxy"
+import url from "url"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/cadastrar-usuario", httpProxy("http://localhost:3002"))
-app.use("/conquista", httpProxy("http://localhost:3003"));
 
+app.use("/desbloquear-conquista", httpProxy("http://localhost:3003/desbloquear-conquista"));
 
+const receitaProxy = httpProxy('http://localhost:3004/', {
+  proxyReqPathResolver: req => url.parse(req.baseUrl).path
+});
+
+app.use('/receita/:id', receitaProxy);
+app.use('/receitas', receitaProxy);
 
 export default app;
--- packages/receita/README.md
+++
@@ -1,1 +0,0 @@
-# receitinha-backend
