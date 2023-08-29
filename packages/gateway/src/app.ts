863f979713e6dd58eb21a53fd5c543dc91acfcec
apfdamascena <apfd@cin.ufpe.br>
2023-08-28 22:59:54 -0300

    feature(login-microservice): add all folders to gateway

--- packages/gateway/src/app.ts
+++ packages/gateway/src/app.ts
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
 app.use("/cadastrar-usuario", httpProxy("http://localhost:3002"))
 
 app.use("/desbloquear-conquista", httpProxy("http://localhost:3003/desbloquear-conquista"));
